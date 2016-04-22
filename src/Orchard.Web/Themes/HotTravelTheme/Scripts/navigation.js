eb849c14bfa07ace6d3bc5dfb8d2fdb0719c4489
hanjia2014 <hanj@datacom.co.nz>
2016-04-23 11:06:33 +1200

    updated navigation.js

--- src/Orchard.Web/Themes/HotTravelTheme/Scripts/navigation.js
+++ src/Orchard.Web/Themes/HotTravelTheme/Scripts/navigation.js
@@ -4,14 +4,14 @@ topul.className = "nav navbar-nav navbar-right";
 var topNodes = topul.children;
 for (var i = 0; i < topNodes.length; i++) {
     var node = topNodes[i];
-    if (node.children.length > 0) {
+    if (node.children.length > 1) {
         node.className = "dropdown show-on-hover";
         for (var j = 0; j < node.children.length; j++) {
             var child = node.children[j];
-            if (child.tagName == "a") {
+            if (child.tagName == "A") {
                 child.className = "dropdown-toggle";
             }
-            if (child.tagName == "ul") {
+            if (child.tagName == "UL") {
                 child.className = "dropdown-menu";
             }
         }
