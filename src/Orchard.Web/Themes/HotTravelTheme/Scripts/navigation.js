60b4873966998757035e966378fe78f585179873
hanjia2014 <hanj@datacom.co.nz>
2016-04-22 16:22:31 +1200

    menu changes

---
+++ src/Orchard.Web/Themes/HotTravelTheme/Scripts/navigation.js
@@ -0,0 +1,19 @@
+﻿var naviBar = document.getElementById("navbar-main");
+var topul = naviBar.children[0];
+topul.className = "nav navbar-nav navbar-right";
+var topNodes = topul.children;
+for (var i = 0; i < topNodes.length; i++) {
+    var node = topNodes[i];
+    if (node.children.length > 0) {
+        node.className = "dropdown show-on-hover";
+        for (var j = 0; j < node.children.length; j++) {
+            var child = node.children[j];
+            if (child.tagName == "a") {
+                child.className = "dropdown-toggle";
+            }
+            if (child.tagName == "ul") {
+                child.className = "dropdown-menu";
+            }
+        }
+    }
+}
