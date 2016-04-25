fae06024e999d9c16b43173f2a8e2655a22533b6
hanjia2014 <hanj@datacom.co.nz>
2016-04-26 09:20:43 +1200

    navigation style changes

--- src/Orchard.Web/Themes/HotTravelTheme/Scripts/navigation.js
+++ src/Orchard.Web/Themes/HotTravelTheme/Scripts/navigation.js
@@ -10,9 +10,30 @@ for (var i = 0; i < topNodes.length; i++) {
             var child = node.children[j];
             if (child.tagName == "A") {
                 child.className = "dropdown-toggle";
+                child.setAttribute("data-toggle", "dropdown");
             }
             if (child.tagName == "UL") {
                 child.className = "dropdown-menu";
+                //level 2 sub menu
+                var lvl2children = child.children;
+                for (var k = 0; k < lvl2children.length; k++) {
+                    var lvl2child = lvl2children[k];
+                    if (lvl2child.tagName == "LI") {
+                        if (lvl2child.children.length > 1) {
+                            lvl2child.className = "dropdown-submenu show-on-hover";
+                            for (var l = 0; l < lvl2child.children.length; l++) {
+                                var lvl2childsub = lvl2child.children[l];
+                                if (lvl2childsub.tagName == "A") {
+                                    lvl2childsub.className = "dropdown-toggle";
+                                    lvl2childsub.setAttribute("data-toggle", "dropdown");
+                                }
+                                if (lvl2childsub.tagName == "UL") {
+                                    lvl2childsub.className = "dropdown-menu";
+                                }
+                            }
+                        }
+                    }
+                }
             }
         }
     }
