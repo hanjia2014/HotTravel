33784508cd6b1e36044fdc98b4c115ca08bf3b93
hanjia2014 <hanj@datacom.co.nz>
2016-05-06 09:10:54 +1200

    set text length

--- src/Orchard.Web/Modules/HotTravel.InformationCard/Models/InformationCard.cs
+++ src/Orchard.Web/Modules/HotTravel.InformationCard/Models/InformationCard.cs
@@ -1,5 +1,6 @@
 ﻿using Orchard.ContentManagement;
 using Orchard.ContentManagement.Records;
+using System.ComponentModel.DataAnnotations;
 
 namespace HotTravel.InformationCard.Models
 {
@@ -16,6 +17,7 @@ namespace HotTravel.InformationCard.Models
     {
         public bool IsWider { get { return Retrieve(p => p.IsWider); } set { Store(p => p.IsWider, value); } }
         public string Title { get { return Retrieve(p => p.Title); } set { Store(p => p.Title, value); } }
+        [StringLength(5000)]
         public string Text { get { return Retrieve(p => p.Text); } set { Store(p => p.Text, value); } }
         public string Location { get { return Retrieve(p => p.Location); } set { Store(p => p.Location, value); } }
         public string LinkedPage { get { return Retrieve(p => p.LinkedPage); } set { Store(p => p.LinkedPage, value); } }
