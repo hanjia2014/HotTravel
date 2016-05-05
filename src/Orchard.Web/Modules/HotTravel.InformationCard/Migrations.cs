e3a6a4f0a1da473414a9a6b9c6707d0dbe3f3ea8
hanjia2014 <hanj@datacom.co.nz>
2016-05-05 15:25:56 +1200

    change length of text column

--- src/Orchard.Web/Modules/HotTravel.InformationCard/Migrations.cs
+++ src/Orchard.Web/Modules/HotTravel.InformationCard/Migrations.cs
@@ -6,27 +6,29 @@ using HotTravel.InformationCard.Models;
 
 namespace HotTravel.InformationCard
 {
-    public class Migrations : DataMigrationImpl {
+    public class Migrations : DataMigrationImpl
+    {
 
-        public int Create() {
-			// Creating table InformationCardPartRecord
-			SchemaBuilder.CreateTable("InformationCardPartRecord", table => table
-				.ContentPartRecord()
-				.Column("IsWider", DbType.Boolean)
-				.Column("Title", DbType.String)
-				.Column("Text", DbType.String)
-				.Column("Location", DbType.String)
-				.Column("LinkedPage", DbType.String)
-			);
+        public int Create()
+        {
+            // Creating table InformationCardPartRecord
+            SchemaBuilder.CreateTable("InformationCardPartRecord", table => table
+                .ContentPartRecord()
+                .Column("IsWider", DbType.Boolean)
+                .Column("Title", DbType.String)
+                .Column("Text", DbType.String, x => x.Unlimited())
+                .Column("Location", DbType.String)
+                .Column("LinkedPage", DbType.String)
+            );
             ContentDefinitionManager.AlterPartDefinition(typeof(InformationCardPart).Name, cfg => cfg.Attachable());
 
             // Creating table InformationCardGroupPartRecord
             SchemaBuilder.CreateTable("InformationCardGroupPartRecord", table => table
-				.ContentPartRecord()
-				.Column("HeaderVisible", DbType.Boolean)
-				.Column("Title", DbType.String)
-				.Column("LinkedPage", DbType.String)
-			);
+                .ContentPartRecord()
+                .Column("HeaderVisible", DbType.Boolean)
+                .Column("Title", DbType.String)
+                .Column("LinkedPage", DbType.String)
+            );
             ContentDefinitionManager.AlterPartDefinition(typeof(InformationCardGroupPart).Name, cfg => cfg.Attachable().WithField("ContentItems", f => f
             .OfType("ContentPickerField").WithSetting("ContentPickerFieldSettings.Multiple", "True")
             .WithDisplayName("Content Items")));
